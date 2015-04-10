775f4c00dbad662cf26f785ec7e7500467f30ab8
Konato_K <konato@bunnycarrot.net>
2015-04-10 02:55:17 -0500

    Add ProjectileListener
    Replaces arrows with snowballs

---
+++ com/mandarinacorp/snowarrows/ProjectileListener.java
@@ -0,0 +1,53 @@
+package com.mandarinacorp.snowarrows;
+
+import org.bukkit.event.entity.EntityShootBowEvent;
+import org.bukkit.metadata.FixedMetadataValue;
+
+import org.bukkit.entity.Projectile;
+import org.bukkit.entity.Snowball;
+import org.bukkit.entity.Arrow;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.HandlerList;
+import org.bukkit.event.Listener;
+
+import java.util.Random;
+
+public final class ProjectileListener implements Listener
+{ private final SnowArrows plugin;
+  private final Random random;
+
+  ProjectileListener(SnowArrows p)
+  { plugin = p;
+    random = new Random();
+  }
+
+  void register()
+  { plugin.getServer().getPluginManager().registerEvents(this, plugin);
+  }
+
+  void unregister()
+  { HandlerList.unregisterAll(this);
+  }
+
+  @EventHandler(ignoreCancelled = true)
+  public void onEntityShootBow(EntityShootBowEvent e)
+  { //I don't think this can be other than arrow, but better be sure anyway
+    if(e.getProjectile() instanceof Arrow)
+    { Arrow arrow = (Arrow)e.getProjectile();
+      //Make a new snowball and fill it with the arrow data, then replace it
+      Projectile snowball = e.getEntity().getWorld().spawn(arrow.getLocation(), Snowball.class);
+      snowball.setFireTicks(arrow.getFireTicks());
+      snowball.setVelocity(arrow.getVelocity());
+      snowball.setShooter(arrow.getShooter());
+      snowball.setBounce(arrow.doesBounce());
+      e.setProjectile(snowball);
+      //Attach the data that snowballs can't have so we can use it later
+      snowball.setMetadata("knockback", new FixedMetadataValue(plugin, arrow.getKnockbackStrength()));
+      //snowball.setMetadata("damage", new FixedMetadataValue(plugin, NmsUtil.getArrowDamage(arrow))); //I'm working on this
+      snowball.setMetadata("critical", new FixedMetadataValue(plugin, arrow.isCritical()));
+      //And attach another metadata just to recognice this is a custom snowball
+      snowball.setMetadata("snowarrow", new FixedMetadataValue(plugin, true));
+    }
+  }
+}
--- com/mandarinacorp/snowarrows/SnowArrows.java
+++ com/mandarinacorp/snowarrows/SnowArrows.java
@@ -3,13 +3,15 @@ package com.mandarinacorp.snowarrows;
 import org.bukkit.plugin.java.*;
 
 public class SnowArrows extends JavaPlugin
-{ @Override
+{ private ProjectileListener listener = new ProjectileListener(this);
+
+  @Override
   public void onEnable()
-  {
+  { listener.register();
   }
 
   @Override
   public void onDisable()
-  {
+  { listener.unregister();
   }
 }
