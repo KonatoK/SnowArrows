cb5c8e5676c246a6c1df85b07d56cf1820ac0f3a
Konato_K <konato@bunnycarrot.net>
2015-04-10 02:44:57 -0500

    Let's start with the basics

---
+++ com/mandarinacorp/snowarrows/SnowArrows.java
@@ -0,0 +1,15 @@
+package com.mandarinacorp.snowarrows;
+
+import org.bukkit.plugin.java.*;
+
+public class SnowArrows extends JavaPlugin
+{ @Override
+  public void onEnable()
+  {
+  }
+
+  @Override
+  public void onDisable()
+  {
+  }
+}
